49713e6d51eb77bc25d29fe3a6f20c6d77520284
benhawker <ben.c.hawker@gmail.com>
2016-07-13 17:53:50 +0800

    Spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require "roomallo_api"
 require "webmock/rspec"
 
-# Disable all net connections except localhost
+# Disable all net connections except localhost. Override this in a spec with ++WebMock.allow_net_connect!++
 WebMock.disable_net_connect!(:allow_localhost => true)
